3e9e29d0136de49754a0f5adde7c73869b935bb1
kenken-oshin <kenken.oshin.87@gmail.com>
2021-03-02 03:20:45 +0900

    updated ongoing project

--- microblog/urls.py
+++ microblog/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import BlogListView
-from blog.views import BlogDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name="index"),
     path('<int:pk>', BlogDetailView.as_view(), name="detail"),
+    path('<int:pk>/update', BlogUpdateView.as_view(), name="update"),
+    path('<int:pk>/delete', BlogDeleteView.as_view(), name="delete"),
+    path('create', BlogCreateView.as_view(), name="create"),
+    path('login', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('admin/', admin.site.urls),
 ]
